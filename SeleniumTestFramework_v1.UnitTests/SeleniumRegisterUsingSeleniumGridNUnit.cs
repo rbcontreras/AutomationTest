62f22a8f69e7984be3ab231d5f9bd5fb16ff6602
rbcontreras <rbcontreras@gmail.com>
2016-10-17 01:08:01 +0800

    cross platform browser using selenium grid

---
+++ SeleniumTestFramework_v1.UnitTests/SeleniumRegisterUsingSeleniumGridNUnit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium.Remote;
+using Test.Models.SeleniumModels;
+
+namespace SeleniumTestFramework_v1.UnitTests
+{
+    public class SeleniumRegisterUsingSeleniumGridNUnit
+    {
+        private IWebDriver _driver;
+        private static TimeSpan INIT_TIMEOUT_SEC = TimeSpan.FromSeconds(180);
+        private static TimeSpan IMPLICIT_TIMEOUT_SEC = TimeSpan.FromSeconds(10);
+
+        [SetUp]
+        public void InitializeTest()
+        {
+            DesiredCapabilities capabilities = DesiredCapabilities.Chrome();
+            _driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), capabilities, INIT_TIMEOUT_SEC);
+
+            _driver.Url = "http://demoqa.com/registration/";
+            _driver.Manage().Timeouts().ImplicitlyWait(IMPLICIT_TIMEOUT_SEC);
+        }
+
+        [Test]
+        public void RegisterInChromeNUnit_Invalid()
+        {
+            var page = new ChromeGoogleSearch(_driver);
+            page.Register();
+            Assert.IsTrue(page.ErrorDisplay.Displayed);
+
+            _driver.Close();
+            _driver.Quit();
+        }
+    }
+}
---
+++ SeleniumTestFramework_v1.UnitTests/SeleniumRegistrationCrossPlatformTest.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Models.SeleniumModels;
+
+namespace SeleniumTestFramework_v1.UnitTests
+{    
+    [TestFixture(typeof(FirefoxDriver))]
+    [TestFixture(typeof(ChromeDriver))]    
+    public class SeleniumRegistrationCrossPlatformTest<TWebDriver> where TWebDriver : IWebDriver, new()
+    {
+        private IWebDriver _driver;
+        private static TimeSpan INIT_TIMEOUT_SEC = TimeSpan.FromSeconds(180);
+        private static TimeSpan IMPLICIT_TIMEOUT_SEC = TimeSpan.FromSeconds(10);
+
+        public SeleniumRegistrationCrossPlatformTest()
+        {
+            Driver<TWebDriver>.Initialize(new Uri("http://localhost:4444/wd/hub"));
+        }
+
+        [SetUp]
+        public void InitializeTest()
+        {
+            _driver = Driver<TWebDriver>.Instance;
+            //DesiredCapabilities capabilities = DesiredCapabilities.Chrome();
+            //_driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), capabilities, INIT_TIMEOUT_SEC);
+            
+            _driver.Url = "http://demoqa.com/registration/";
+            _driver.Manage().Timeouts().ImplicitlyWait(IMPLICIT_TIMEOUT_SEC);            
+        }
+
+        [Test]
+        public void RegisterInChromeNUnitCrossPlatform_Invalid()
+        {
+            var page = new ChromeGoogleSearch(_driver);
+            page.Register();
+            Assert.IsTrue(page.ErrorDisplay.Displayed);
+
+            _driver.Close();
+            _driver.Quit();
+        }
+    }
+
+
+    public class Driver<TWebDriver> where TWebDriver : IWebDriver, new()
+    {
+        public static IWebDriver Instance { get; set; }
+
+        public static void Initialize(Uri remoteAddress)
+        {
+            if (typeof(TWebDriver) == typeof(ChromeDriver))
+            {
+                var browser = DesiredCapabilities.Chrome();
+                Instance = new RemoteWebDriver(remoteAddress, browser);
+            } 
+            else 
+            {
+                DesiredCapabilities browser = DesiredCapabilities.Firefox();
+                Instance = new RemoteWebDriver(remoteAddress, browser);
+            }   
+        }        
+    }
+}
